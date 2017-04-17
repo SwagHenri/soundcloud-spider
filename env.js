3e8459c0c71437cb81a32435dc6ff5591f8920f1
SwagHenri <guillaume.sauvagecombat@gmail.com>
2017-04-18 00:21:34 +0200

    no message

---
+++ env.js
@@ -0,0 +1,10 @@
+var jsonfile = require("jsonfile");
+
+var CONFIG = jsonfile.readFileSync("./config.json");
+
+process.env["HOST"] = CONFIG.host;
+process.env["DBPORT"] = CONFIG.dbport;
+process.env["USERNAME"] = CONFIG.username;
+process.env["PASSWORD"] = CONFIG.password;
+
+
